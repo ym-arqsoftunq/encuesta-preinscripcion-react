a0586cea501f13a4899d3f2209ddd7da09bd9b7f
Nacho Yegro <nachoyegro@gmail.com>
2017-09-25 08:46:01 -0300

    Commit inicial con una lista simple llamando al backend

---
+++ app/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Alumnos from './Alumnos/Alumnos';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+        <div className="App">
+            <Alumnos />
+        </div>
+    );
+  }
+}
+
+export default App;
